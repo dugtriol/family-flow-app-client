fce66ad5a5b299c06cc3c1b6c0000437e2532a44
aimandzhi <aigulmndzh@gmail.com>
2025-04-18 21:05:40 +0300

    Refactor ShoppingWishlistPage app bar for improved UI and localization

--- family_flow_app_client/lib/shopping_wishlist/view/shopping_wishlist_page.dart
+++ family_flow_app_client/lib/shopping_wishlist/view/shopping_wishlist_page.dart
@@ -15,14 +15,34 @@ class ShoppingWishlistPage extends StatelessWidget {
       length: 2, // Количество вкладок
       child: Scaffold(
         appBar: AppBar(
-          title: const Text('Shopping & Wishlist'),
-          bottom: const TabBar(
-            tabs: [
-              Tab(text: 'Shopping'),
-              Tab(text: 'Wishlist'),
+          backgroundColor: Colors.white,
+          elevation: 0,
+          title: Row(
+            children: [
+              const Icon(
+                Icons.shopping_bag,
+                color: Colors.deepPurple,
+                size: 28,
+              ),
+              const SizedBox(width: 8),
+              const Text(
+                'Списки',
+                style: TextStyle(
+                  color: Colors.black87,
+                  fontSize: 20,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
             ],
           ),
+          bottom: const TabBar(
+            indicatorColor: Colors.deepPurple,
+            labelColor: Colors.deepPurple,
+            unselectedLabelColor: Colors.black54,
+            tabs: [Tab(text: 'Покупки'), Tab(text: 'Желания')],
+          ),
         ),
+        backgroundColor: Colors.white, // Устанавливаем цвет фона как у AppBar
         body: TabBarView(
           children: [
             BlocProvider.value(
