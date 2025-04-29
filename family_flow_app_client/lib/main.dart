a88f4a33a2a31f4e8c163eb4a49a2cd99e50b6d7
aimandzhi <aigulmndzh@gmail.com>
2025-04-29 21:09:07 +0300

    Add notification permission request and update user_api dependency

--- family_flow_app_client/lib/main.dart
+++ family_flow_app_client/lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter/widgets.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:permission_handler/permission_handler.dart';
 // import 'package:yandex_maps_mapkit_lite/init.dart' as init;
 
 import 'app/app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await requestNotificationPermission();
 
   await initializeDateFormatting('ru', null);
   Bloc.observer = AppBlocObserver();
@@ -16,4 +18,10 @@ Future<void> main() async {
   // init.initMapkit(apiKey: 'a6f9dfc0-7ab3-44c1-a592-fc6086fd98cd');
 
   runApp(App());
+}
+
+Future<void> requestNotificationPermission() async {
+  if (await Permission.notification.isDenied) {
+    await Permission.notification.request();
+  }
 }
